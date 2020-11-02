fea1c6de452b045f5306ad1e1988bd8c4d83a32e
Wannes Fransen <wannes.fransen@ucll.be>
2020-11-02 14:46:12 +0100

    WIP

--- lib/generated/elixir_assignment_messages_task_response.ex
+++ lib/generated/elixir_assignment_messages_task_response.ex
@@ -2,7 +2,7 @@
 defmodule(AssignmentMessages.TaskResponse) do
   @moduledoc(false)
   (
-    defstruct(task_result: :COMPLETE, task: nil, __uf__: [])
+    defstruct(task_result: :COMPLETE, todo_task_uuid: "", __uf__: [])
     (
       @spec(encode(struct) :: {:ok, iodata} | {:error, any})
       def(encode(msg)) do
@@ -15,7 +15,7 @@ defmodule(AssignmentMessages.TaskResponse) do
       end
       @spec(encode!(struct) :: iodata | no_return)
       def(encode!(msg)) do
-        [] |> encode_task_result(msg) |> encode_task(msg) |> encode_unknown_fields(msg)
+        [] |> encode_task_result(msg) |> encode_todo_task_uuid(msg) |> encode_unknown_fields(msg)
       end
       []
       [defp(encode_task_result(acc, msg)) do
@@ -25,12 +25,12 @@ defmodule(AssignmentMessages.TaskResponse) do
         else
           [acc, "\b", field_value |> AssignmentMessages.TaskResponse.Result.encode() |> Protox.Encode.encode_enum()]
         end
-      end, defp(encode_task(acc, msg)) do
-        field_value = msg.task()
-        if(field_value == nil) do
+      end, defp(encode_todo_task_uuid(acc, msg)) do
+        field_value = msg.todo_task_uuid()
+        if(field_value == "") do
           acc
         else
-          [acc, <<18>>, Protox.Encode.encode_message(field_value)]
+          [acc, <<18>>, Protox.Encode.encode_string(field_value)]
         end
       end]
       defp(encode_unknown_fields(acc, msg)) do
@@ -79,8 +79,8 @@ defmodule(AssignmentMessages.TaskResponse) do
             {2, _, bytes} ->
               {len, bytes} = Protox.Varint.decode(bytes)
               <<delimited::binary-size(len), rest::binary>> = bytes
-              value = AssignmentMessages.TodoTask.decode!(delimited)
-              field = {:task, Protox.Message.merge(msg.task(), value)}
+              value = delimited
+              field = {:todo_task_uuid, value}
               {field, rest}
             {tag, wire_type, rest} ->
               {value, new_rest} = Protox.Decode.parse_unknown(tag, wire_type, rest)
@@ -95,11 +95,11 @@ defmodule(AssignmentMessages.TaskResponse) do
     )
     @spec(defs() :: %{required(non_neg_integer) => {atom, Protox.Types.kind(), Protox.Types.type()}})
     def(defs()) do
-      %{1 => {:task_result, {:default, :COMPLETE}, {:enum, AssignmentMessages.TaskResponse.Result}}, 2 => {:task, {:default, nil}, {:message, AssignmentMessages.TodoTask}}}
+      %{1 => {:task_result, {:default, :COMPLETE}, {:enum, AssignmentMessages.TaskResponse.Result}}, 2 => {:todo_task_uuid, {:default, ""}, :string}}
     end
     @spec(defs_by_name() :: %{required(atom) => {non_neg_integer, Protox.Types.kind(), Protox.Types.type()}})
     def(defs_by_name()) do
-      %{task: {2, {:default, nil}, {:message, AssignmentMessages.TodoTask}}, task_result: {1, {:default, :COMPLETE}, {:enum, AssignmentMessages.TaskResponse.Result}}}
+      %{task_result: {1, {:default, :COMPLETE}, {:enum, AssignmentMessages.TaskResponse.Result}}, todo_task_uuid: {2, {:default, ""}, :string}}
     end
     @spec(required_fields() :: [])
     def(required_fields()) do
@@ -123,8 +123,8 @@ defmodule(AssignmentMessages.TaskResponse) do
     end
     [@spec(default(atom) :: {:ok, boolean | integer | String.t() | float} | {:error, atom}), [def(default(:task_result)) do
       {:ok, :COMPLETE}
-    end, def(default(:task)) do
-      {:ok, nil}
+    end, def(default(:todo_task_uuid)) do
+      {:ok, ""}
     end], def(default(_)) do
       {:error, :no_such_field}
     end]
